dfedf1a618a672c17adeed59a74125beae427e56
Maisu <mystasly48@gmail.com>
2016-09-13 16:09:35 +0900

    Initial Commit

---
+++ ClickGame/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ClickGame {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.score = new System.Windows.Forms.Label();
+            this.remaining = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Location = new System.Drawing.Point(45, 24);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(11, 12);
+            this.score.TabIndex = 1;
+            this.score.Text = "0";
+            // 
+            // remaining
+            // 
+            this.remaining.AutoSize = true;
+            this.remaining.Location = new System.Drawing.Point(201, 24);
+            this.remaining.Name = "remaining";
+            this.remaining.Size = new System.Drawing.Size(17, 12);
+            this.remaining.TabIndex = 2;
+            this.remaining.Text = "60";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 212);
+            this.Controls.Add(this.remaining);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label remaining;
+    }
+}
+
---
+++ ClickGame/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickGame {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        bool IsRunning = false;
+        int Remaining = 60;
+        int Count = 0;
+
+        private void button1_Click(object sender, EventArgs e) {
+            var count = int.Parse(remaining.Text);
+            remaining.Text = (count++).ToString();
+            if (!IsRunning) {
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            var count = int.Parse(remaining.Text);
+            remaining.Text = (count--).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            score.Text = "60";
+            remaining.Text = "0";
+            button1.Text = "Click!";
+        }
+    }
+}
